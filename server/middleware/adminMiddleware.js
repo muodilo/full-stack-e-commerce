22555988699e83c40162a2fe78106719b35589e8
odilo <odilomurindahabi@gmail.com>
2024-05-11 13:52:01 +0200

    create admin middleware

---
+++ server/middleware/adminMiddleware.js
@@ -0,0 +1,17 @@
+const asyncHandler = require("express-async-handler");
+
+const checkAdmin = asyncHandler(async (req, res, next) => {
+  if (!req.user) {
+    res.status(401);
+    throw new Error("Not Authorized");
+  }
+  const user = req.user;
+  if (user && user.role === "admin") {
+    next();
+  } else {
+    res.status(401)
+    throw new Error('Not Authoried as admin')
+  }
+})
+
+module, exports = checkAdmin;
