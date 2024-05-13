714370d75df6be7ad1a366c5eb502b92c0616637
odilo <odilomurindahabi@gmail.com>
2024-05-13 15:54:21 +0200

    install redux

---
+++ client/src/app/store.js
@@ -0,0 +1,5 @@
+import { configureStore } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
+import { persistReducer } from 'redux-persist';
+import { combineReducers } from '@reduxjs/toolkit';
+
