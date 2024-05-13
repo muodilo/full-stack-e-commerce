e991ba4cdb602bb455f4006efb3ddecfc5faa023
odilo <odilomurindahabi@gmail.com>
2024-05-13 15:39:23 +0200

    create remove from cart controller and route

--- server/controllers/cartController.js
+++ server/controllers/cartController.js
@@ -5,7 +5,7 @@ const User = require('../models/userModel.js');
 
 
 const addToCart = asyncHandler(async (req, res) => {
-  const { productId} = req.body;
+  const { productId } = req.body;
   const userId = req.user._id; // Assuming you have user information in req.user
   
   try {
@@ -21,64 +21,92 @@ const addToCart = asyncHandler(async (req, res) => {
     const index = cart.items.findIndex(item => item.product.toString() === productId);
     
     if (index !== -1) {
-      
       // If the product is already in the cart, update the quantity
       cart.items[index].quantity += 1;
     } else {
       // If the product is not in the cart, add it
-      cart.items.push({ product: productId, quantity:1 });
+      cart.items.push({ product: productId, quantity: 1 });
     }
     
     // Save the updated cart
     await cart.save();
     
-    res.status(200).json('Product added to cart successfully');
+    // Get the number of unique items in the cart
+    const uniqueItems = new Set(cart.items.map(item => item.product.toString()));
+    const numberOfUniqueItems = uniqueItems.size;
+    
+    res.status(200).json({ message: 'Product added to cart successfully', numberOfUniqueItems });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 });
+
+const removeFromCart = asyncHandler(async (req, res) => {
+  const { productId } = req.body;
+  const userId = req.user._id; // Assuming you have user information in req.user
+  
+  try {
+    // Find the user's cart
+    let cart = await Cart.findOne({ user: userId });
+    
+    // If the user doesn't have a cart, return an error
+    if (!cart) {
+      res.status(404).json({ message: 'Cart not found' });
+      return;
+    }
+    
+    // Find the index of the item to remove
+    const index = cart.items.findIndex(item => item.product.toString() === productId);
+    
+    // If the item is not found in the cart, return an error
+    if (index === -1) {
+      res.status(404).json({ message: 'Item not found in cart' });
+      return;
+    }
+    
+    // Remove the item from the cart's items array
+    cart.items.splice(index, 1);
+    
+    // Save the updated cart
+    await cart.save();
+    
+    res.status(200).json({ message: 'Item removed from cart successfully' });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+
 
 
 const getCartDetails = asyncHandler(async (req, res) => {
   const userId = req.user._id; // Assuming you have user information in req.user
   
   try {
-    // Find the user's cart
-    const cart = await Cart.findOne({ user: userId }).populate('items.product');
+    // Find the user's cart and populate product details
+    const cart = await Cart.findOne({ user: userId }).populate({
+      path: 'items.product',
+      select: 'name description price discountPrice category type quantity images',
+    });
     
     if (!cart) {
       res.status(404).json({ message: 'Cart not found' });
       return;
     }
 
-    // Extract details from the cart
-    const cartDetails = {
-      user: cart.user,
-      items: cart.items.map(item => ({
-        product: {
-          _id: item.product._id,
-          name: item.product.name,
-          description: item.product.description,
-          price: item.product.price,
-          discountPrice: item.product.discountPrice,
-          category: item.product.category,
-          type: item.product.type,
-          quantityAvailable: item.product.quantity,
-          images: item.product.images,
-        },
-        quantity: item.quantity
-      }))
-    };
+    const numberOfItems = cart.items.length;
 
-    res.status(200).json({ cart: cartDetails });
+    res.status(200).json({ cart, numberOfItems });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 });
 
 
+
 module.exports = {
   addToCart,
   getCartDetails,
-  
+  removeFromCart,
+
 }
--- server/routes/cartRoutes.js
+++ server/routes/cartRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { addToCart,getCartDetails } = require('../controllers/cartController.js');
+const { addToCart,getCartDetails,removeFromCart } = require('../controllers/cartController.js');
 const protect = require('../middleware/authMiddleware.js');
 const router = express.Router();
 
 router.post('/', protect, addToCart);
-router.get('/', protect,getCartDetails);
+router.get('/', protect, getCartDetails);
+router.delete('/remove', protect, removeFromCart);
 
 module.exports = router;
