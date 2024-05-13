c754a61a70f92a090a4cab437c9393a7fa544d5a
odilo <odilomurindahabi@gmail.com>
2024-05-13 16:52:39 +0200

    create register service

---
+++ client/src/features/auth/authServices.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+const BASE_API_URL = import.meta.env.REACT_APP_API_URL;
+
+const register = async (userData) => {
+  const response = await axios.post(`${BASE_API_URL}/users`, userData);
+
+  if (response.data) {
+    localStorage.setItem('user', JSON.stringify(response.data));
+  }
+
+  return response.data;
+};
+
+
+const authService = {
+  register,
+}
+
+export default authService;
