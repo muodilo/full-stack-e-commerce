86a72daa5da038f1485edc2e81282b36b5dabbde
odilo <odilomurindahabi@gmail.com>
2024-05-13 11:12:20 +0200

    create getAllProducts controller

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { createProduct,updateProduct,deleteProduct } = require('../controllers/productController.js');
+const { createProduct,updateProduct,deleteProduct,getAllProducts } = require('../controllers/productController.js');
 const protect = require('../middleware/authMiddleware.js');
 const checkAdmin  = require('../middleware/adminMiddleware.js');
 
@@ -8,5 +8,6 @@ const router = express.Router();
 router.post('/', protect, checkAdmin, createProduct);
 router.put('/:id', protect, checkAdmin, updateProduct);
 router.delete('/:id', protect, checkAdmin, deleteProduct);
+router.get('/', getAllProducts);
 
 module.exports = router;
