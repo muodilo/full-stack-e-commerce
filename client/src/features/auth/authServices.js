47cf77122e59a226b1f5b0107bd21a19ad84e76b
odilo <odilomurindahabi@gmail.com>
2024-05-14 18:41:46 +0200

    create login service

--- client/src/features/auth/authServices.js
+++ client/src/features/auth/authServices.js
@@ -14,10 +14,20 @@ const register = async (userData) => {
 
 const logout = async () => localStorage.removeItem('user');
 
+const login = async (userData) => {
+  const response = await axios.post(`${BASE_API_URL}/users/login`, userData);
+
+  if (response.data) {
+    localStorage.setItem('user', JSON.stringify(response.data));
+  }
+  return response.data;
+}
+
 
 const authService = {
   register,
-  logout
+  logout,
+  login
 }
 
 export default authService;
