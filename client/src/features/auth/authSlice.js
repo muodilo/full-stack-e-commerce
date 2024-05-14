5f315b7c749192cd85d0c4c0074c82b30a932cfd
odilo <odilomurindahabi@gmail.com>
2024-05-14 18:45:36 +0200

    create login action

--- client/src/features/auth/authSlice.js
+++ client/src/features/auth/authSlice.js
@@ -22,6 +22,17 @@ export const register = createAsyncThunk('auth/register', async (userData,thunkA
   }
 })
 
+//Login a user
+export const login = createAsyncThunk('auth/login', async (userData, thunkAPI) => {
+  try {
+    return await authService.login(userData);
+  } catch (error) {
+    const message = (error.response && error.response.data && error.response.data.message) || error.message || error.toString();
+
+    return thunkAPI.rejectWithValue(message);
+  }
+})
+
 export const authSlice = createSlice({
   name: 'auth',
   initialState,
@@ -48,6 +59,20 @@ export const authSlice = createSlice({
       state.isError = true
       state.user = null
       state.message = action.payload
+    })
+    .addCase(login.pending, (state) => {
+      state.isLoading = true;
+    })
+    .addCase(login.fulfilled, (state,action) => {
+      state.isLoading = false
+      state.isSuccess = true
+      state.user = action.payload
+    })
+    .addCase(login.rejected, (state,action) => {
+      state.isLoading = false
+      state.isError = true
+      state.message = action.payload
+      state.user = null
     })
   }
 })
