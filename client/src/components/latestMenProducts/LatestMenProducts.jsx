f4c5e493be30f9fc64601ac998998d7bac211a4b
odilo <odilomurindahabi@gmail.com>
2024-05-15 12:10:29 +0200

    fetch latestMenProducts

--- client/src/components/latestMenProducts/LatestMenProducts.jsx
+++ client/src/components/latestMenProducts/LatestMenProducts.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 import { BsFillCartPlusFill } from "react-icons/bs";
 import { BsFillCartCheckFill } from "react-icons/bs";
+import { getLatestMenProducts,resetProduct } from '../../features/products/productSlice';
+import { useDispatch,useSelector } from 'react-redux';
 
 const LatestMenProducts = () => {
+
+  const dispatch = useDispatch();
+  const { latestMen, lastestMenAreLoading, lastestMenError, latestMenSuccess, latestMenMessage } = useSelector(state => state.reducer.product);
+  
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        await dispatch(getLatestMenProducts());
+        dispatch(resetProduct());
+      } catch (error) {
+        console.error(error);
+        dispatch(resetProduct());
+      }
+    }
+    fetchData();
+  },[dispatch])
   return (
     <section>
       <div className='lg:px-[7rem] md:px-[5rem] px-2 pt-20'>
